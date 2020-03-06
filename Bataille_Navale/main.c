7423b2a0f42a49fb6f334d1dfde2697eb4cb86ff
volerysven <sven.volery@cpnv.ch>
2020-03-06 14:44:04 +0100

    code CLions
    Void menu, void aide, switch et début du void jouer

--- Bataille_Navale/main.c
+++ Bataille_Navale/main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#pragma execution_character_set("utf-8")
 
 
-int main() {
+void jouer(){
+    printf("Game");
+}
+
+void aide(){
+    FILE* fichier = NULL;
+    int caractereActuel = 0;
+
+    fichier = fopen("aide.txt", "r");
+
+    if (fichier != NULL)
+    {
+        do
+        {
+            caractereActuel = fgetc(fichier);
+            printf("%c", caractereActuel);
+        }
+        while (caractereActuel != EOF);
+
+        fclose(fichier);
+    }
+    return 0;
+}
+
+void menu(){
+    int choixmenu;
+
     printf("Bataille Navale\n\n");
 
     printf("1 - Jouer \n");
@@ -9,11 +38,41 @@ int main() {
     printf("3 - S'authentifier \n");
     printf("4 - Aide \n");
     printf("5 - Quitter \n");
+    scanf("%d", &choixmenu);
 
 
+    switch(choixmenu){
+        case 1: jouer();
+            break;
+
+        case 2:
+            printf("Scores\n");
+            break;
+
+        case 3:
+            printf("S'authentifier\n");
+            break;
+
+        case 4: aide();
+            break;
+
+        case 5:
+            break;
+
+        default:system("cls");
+                menu();
+                break;
+    }
+}
+
+int main() {
+    SetConsoleOutputCP(65001);
+
+   //appeler fonction menu
+   menu();
 
 
-
-
+   printf("\n\n");
+   system("pause");
     return 0;
 }
