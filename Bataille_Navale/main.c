595aec6dfb9810b1bfab270e34b9278348624e53
volerysven <sven.volery@cpnv.ch>
2020-03-06 11:35:48 +0100

    Création du ficher CLions pour la bataille navale

---
+++ Bataille_Navale/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main() {
+    printf("Bataille Navale\n\n");
+
+    printf("1 - Jouer \n");
+    printf("2 - Scores \n");
+    printf("3 - S'authentifier \n");
+    printf("4 - Aide \n");
+    printf("5 - Quitter \n");
+
+
+
+
+
+
+    return 0;
+}
